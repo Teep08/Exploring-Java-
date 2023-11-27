f6c07caf4b6ffedc9264217f52bba19b4681d086
Deepak Dobhal <55650178+Teep08@users.noreply.github.com>
2023-11-27 21:34:12 +0530

    Function
    In this program we will see how we can create a function in java and how we get the value from our function (return) and also we how we can give input (Arguments) to our function

---
+++ Function.java
@@ -0,0 +1,19 @@
+public class Function {
+
+    public static int cal(int a,int b){
+        int add = a+b;
+        return add;
+    }
+
+    public static void main(String args[]) {
+
+        int i=2;
+        int j=6;
+
+        int sum= cal(i,j);
+
+        System.out.println("Sum is :"+sum);
+
+    }
+    
+}
