1d224cc626d813f20d6a14859f479ba0cce1ae11
Deepak Dobhal <55650178+Teep08@users.noreply.github.com>
2024-01-05 23:47:38 +0530

    StringBuilder
    Hi , in this program we will see the concept about the StringBuilder , In java String is immutable (Unchangeable) , but StringBuilder is mutable (Changeable).
    if we want to edit our string then we need to use StringBuilder.
    In this program we will see the syntax of StringBuilder , how we can use it and some inbuild functions of StringBuilder.

---
+++ StringBuild.java
@@ -0,0 +1,15 @@
+public class StringBuild {
+    public static void main(String args[])
+    {
+        StringBuilder str = new StringBuilder("Hello");
+        System.out.println(str);  //Hello
+
+        //for Amend the string
+        str.append("Guys");
+        System.out.println(str);    //original string is changed now output is HelloGuys 
+
+        //reverse the string
+        str.reverse();
+        System.out.println(str);    //orignal string is - HelloGuys and after reverse output is syuGolleH
+    }  
+}
