ac222779a72c9a5de6e5e827df7df515921d054e
Deepak Dobhal <55650178+Teep08@users.noreply.github.com>
2024-02-08 01:00:04 +0530

    Guess the Number
    Hi, Today we will play the game.
    
    How to play :
    Step 1 : Program generates 8 random number between 0 to 10.
    Step 2 : You need to input 3 numbers.
    Step 3 : If your input numbers matched with randomly generated 8 number then you are the winner.
    
    How it work :
    Step 1 : Created an array and stored the 8 random number under that array.
    Step 2 : Created a temporary variable with 0 value
    Step 3 : User input the number.
    Step 4 : Check the number , if it's matched with any array number then it's increase the value of temporary variable. 
    Step 5 : Repeat the Step 3 , until user input the 3 values.
    Step 6 : Check the value of temporary variable , if value is equal to or greater then 3 then user won otherwise user lose the game.
    Step 7 : Exit

---
+++ Guessnumber.java
@@ -0,0 +1,62 @@
+// Genereated 8 numbers by program.
+// User input 3 numbers.
+// If user input number present under 8 random number array then he/she will be winner otherwise looser.
+
+import java.util.*;
+public class Guessnumber {
+
+    public static void gen(int ran[])
+    {
+        //Random class object generate 
+        Random ra = new Random();
+        for(int i=0;i<ran.length;i++)
+        {
+            ran[i] = ra.nextInt(10); //It's generate the random numbers and store that under array
+        }
+        guess(ran); //calling the Guess method
+    }
+
+    public static void guess(int rand[])
+    {
+        Scanner scr = new Scanner(System.in);
+        int a=0; 
+        for(int i=1;i<=3;i++)  //input the 3 numbers by user
+        {
+            System.out.print("Guess your "+i+" number :");
+            int guss = scr.nextInt();
+            for( int j :rand)
+            {
+                if(guss == j)  //checking user number is matched with array numbers
+                {
+                    a++; //increase the value of a varaible if number matched
+                }
+            }
+        }
+        if(a>3)
+        {
+            System.out.println("Congratulations you are the winner :)");
+        }
+        else
+        {
+            System.out.println("Sorry better luck next time :(");
+        }
+        System.out.print("The Random numbers are: "); //print the numbers of array
+        for( int j :rand)
+            {
+                System.out.print(j+" ");
+            }
+    }
+    public static void main(String args[])
+    {
+        int ran[] = new int[8];
+        gen(ran); 
+    }
+}
+
+/* OUTPUT : 
+GGuess your 1 number :2
+Guess your 2 number :7
+Guess your 3 number :4
+Sorry better luck next time :(
+The Random numbers are: 7 1 2 3 9 1 7 3
+ */
