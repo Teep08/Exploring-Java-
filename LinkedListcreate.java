449e845eb7ce2326a3e32ca017b83226a534a71f
Deepak Dobhal <55650178+Teep08@users.noreply.github.com>
2024-07-16 21:48:22 +0530

    Linked List creation
    Hi guys, today we will see how we can create a Node in our Linked list.
    
    - Linked list is a list which contains the nodes and nodes are connected with each other.
    - Nodes having a 2 part :
    1st part holds the data
    2nd part holds the address of next node.

---
+++ LinkedListcreate.java
@@ -0,0 +1,14 @@
+public class LinkedListcreate {
+    //create a node class
+    public class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
