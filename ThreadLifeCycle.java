6a7c0110f1a56a2074b697d079eaf9fb70544285
Deepak Dobhal <deepakdobhal08@gmail.com>
2025-11-06 22:04:27 +0530

    Example of Thread life cycle

---
+++ ThreadLifeCycle.java
@@ -0,0 +1,28 @@
+class Threading extends Thread
+{
+    public void run()
+    {
+        System.out.println("Thread Stage Running, run() method executing");
+        try{
+            Thread.sleep(2000);
+            System.out.println("Thread Stage Non Runnable, sleep() method executing");
+        }catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
+
+public class ThreadLifeCycle
+{
+    public static void main(String args[])
+    {
+        Threading t = new Threading();  //New stage
+        System.out.println("Thread stage is New, Object created");
+
+        System.out.println("Thread stage is Runnable, run() method call");
+        t.start();  //runnable stage
+        
+        //Thread stage is Terminated
+    }
+}
